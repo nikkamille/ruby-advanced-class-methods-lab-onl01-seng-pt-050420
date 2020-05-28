92c771063135e77c8f79dfd17a823a7826265a34
Kamille Longalong <k.pagibigan@gmail.com>
2020-05-28 07:34:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,14 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
   end
 
 end
