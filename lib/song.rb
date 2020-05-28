de19082656f3e46f557681032996148eaf1a029b
Kamille Longalong <k.pagibigan@gmail.com>
2020-05-28 07:20:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,8 +41,9 @@ class Song
     self.all.sort_by { |song| song.name }
   end
   
-  def self.new_from_filename
-    
+  def self.new_from_filename(filename)
+    song = song.new 
+    song.name = 
   end
 
 end
